c96b61c3a95e3c3a6f7860383e8e7dd26e26d600
lakshmitgpl <142137737+lakshmitgpl@users.noreply.github.com>
2024-01-27 21:57:47 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker pull lakshmitgpl/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 6000:6000 abhishekf5/simple-python-flask-app
+docker run -d -p 6000:6000 lakshmitgpl/simple-python-flask-app
